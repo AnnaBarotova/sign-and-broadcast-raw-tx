1e6db50adcc5376826c0a9773a25d30c3990c3da
AnnaBarotova <129748746+AnnaBarotova@users.noreply.github.com>
2024-10-16 10:37:12 +0200

    MW testing - Bulk operations with csv as input

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,7 @@ COPY . .
 # Set environment variables
 ENV RAW_TRANSACTION="0x"
 ENV PRIVATE_KEY="0x"
-ENV RPC_URL="https://rpc.ankr.com/eth_goerli"
+ENV RPC_URL="https://ethereum-holesky-rpc.publicnode.com"
 ENV GAS_LIMIT=1000000
 ENV MAX_FEE_PER_GAS_IN_GWEI=50
 ENV MAX_PRIORITY_FEE_IN_GWEI=1
---
+++ sign-and-broadcastFromCsv.js
@@ -0,0 +1,49 @@
+import csv from "csv-parser";
+import fs from "fs"
+import {signAndBroadcast} from "./sign-and-broadcast-raw-tx.js";
+
+async function runSignAndBroadcast(){
+  fs.writeFileSync('tx-list.csv', ''); // Очищаем файл перед записью
+
+  let envvars = {
+    rawTx: '',
+    privateKey: '',
+    rpcUrl: "https://ethereum-holesky-rpc.publicnode.com",
+    valueInEther: "32",
+    gasLimit: "1000000",
+    maxFeePerGasInGwei: "50",
+    maxPriorFeeInGwei: "1"
+  }
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  const results = [];
+
+
+  // Читаем CSV файл
+  fs.createReadStream('address-key-rawTx.csv')
+    .pipe(csv({ separator: ';', headers: ['address', 'privateKey', 'rawTx'] }))
+    .on('data', (row) => {
+      results.push(row); // Собираем все строки
+    })
+    .on('end', async () => {
+      console.log('CSV processing completed, starting to sign');
+      for (const row of results) {
+        let accountData = {
+          address: row.address,
+          privateKey: row.privateKey,
+          rawTx: row.rawTx
+        };
+        envvars.privateKey=accountData.privateKey;
+        envvars.rawTx=accountData.rawTx;
+
+        fs.appendFileSync('tx-list.csv', `${accountData.address};`);
+
+        await signAndBroadcast(envvars)
+        await delay(1500);
+      }
+      console.log('All requests completed.');
+    });
+}
+runSignAndBroadcast();
